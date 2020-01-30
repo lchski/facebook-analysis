238cbda77b4a71d5cbf0787ba2799cf95c5221c3
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-01-29 20:56:24 -0500

    IDs, timezone fixes

--- lib/helpers.R
+++ lib/helpers.R
@@ -7,14 +7,19 @@ read_message_folder <- function(message_folder) {
               unnest(cols = c(sender_name, timestamp_ms, content, type)),
             .id = "source"
     ) %>%
+    mutate(source = str_remove(source, fixed(message_folders_prefix))) %>%
+    separate(source, into = c("source_folder", "source_file"), sep = "/") %>%
     mutate(
       timestamp = as_datetime(as.POSIXct(
         timestamp_ms / 1000,
         origin = "1970-01-01",
-        timezone = "America/Toronto"
-      ))
+        tz = "America/Toronto"
+      ), tz = "America/Toronto")
     ) %>%
-    mutate(source = str_remove(source, fixed(message_folders_prefix)))
+    mutate_at(vars(timestamp), list(year = year, month = month, day = day, hour = hour, minute = minute)) %>%
+    arrange(timestamp) %>%
+    mutate(msg_id = row_number()) %>%
+    select(msg_id, source_folder, source_file, timestamp_ms, timestamp:minute, sender_name, type, content)
 }
 
 
