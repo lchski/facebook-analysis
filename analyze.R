bfd8cd0a07ae1eae57cf78c5369c60ef6387259b
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-01-29 21:08:53 -0500

    some analysisssss

--- analyze.R
+++ analyze.R
@@ -1,3 +1,25 @@
 library(tidytext)
 
+gce_msgs
 
+gce_msgs_words <- gce_msgs %>% unnest_tokens(word, content)
+
+gce_msgs %>%
+  left_join(
+    gce_msgs_words %>%
+      group_by(msg_id) %>%
+      summarize(count = n())
+  )
+
+gce_msgs %>%
+  left_join(
+    gce_msgs %>%
+      unnest_tokens(word, content) %>% ## can also be `gce_msgs_words`
+      group_by(msg_id) %>%
+      summarize(count = n())
+  ) %>%
+  group_by(sender_name) %>%
+  summarize(messages = n(), words = sum(count, na.rm = TRUE)) %>%
+  mutate(messages_prop = messages / sum(messages), words_prop = words / sum(words)) %>%
+  arrange(-words_prop) %>%
+  select(sender_name, messages, messages_prop, words, words_prop)
